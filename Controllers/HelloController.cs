da3b3336f26c3fd3b8fa34e8c8afcd271add0ce3
Hanzo <obtmjd@gmail.com>
2024-02-25 11:00:14 -0400

    first commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello, World!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactFrontend",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000") // Update this with your React app's URL
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowReactFrontend");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
